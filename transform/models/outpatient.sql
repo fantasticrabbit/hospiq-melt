274450af44e0d125c8c181db7a9a4fd07d756a89
Tom Riley <thomriley@gmail.com>
2021-09-10 18:03:52 -0500

    more env vars for dbt

--- transform/models/outpatient.sql
+++ transform/models/outpatient.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 select v.visitkey,v.patientkey,s.urgency,s.procedurename,s.surgeon,arrival,wheelsin,wheelsout,departure
-    from `noble-return-324620.hospiq.visits` as v 
-    join `noble-return-324620.hospiq.surgeries` as s
+        from `{{ env_var('TARGET_BIGQUERY_PROJECT_ID') }}.hospiq.visits` as v 
+        join `{{ env_var('TARGET_BIGQUERY_PROJECT_ID') }}.hospiq.surgeries` as s
     on v.visitkey=s.visitkey 
     where patienttype='OUTPATIENT'
